a3ae610ab4cd1b211616fc9887cd5ad4a6c10db7
John Machado <jrmachado@electro-matic.com>
2021-07-12 18:52:25 -0400

    testing external script

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// define the callAPI function that takes a first name and last name as parameters
+var callAPI = (firstName,lastName)=>{
+    document.getElementById("lname").value = 'newname';
+    // // instantiate a headers object
+    // var myHeaders = new Headers();
+    // // add content type header to object
+    // myHeaders.append("Content-Type", "application/json");
+    // // using built in JSON utility package turn object to string and store in a variable
+    // var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+    // // create a JSON object with parameters for API call and store in a variable
+    // var requestOptions = {
+    //     method: 'POST',
+    //     headers: myHeaders,
+    //     body: raw,
+    //     redirect: 'follow'
+    // };
+    // // make API call with parameters and use promises to get response
+    // fetch("https://bpr3fcc8ua.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+    // .then(response => response.text())
+    // .then(result => alert(JSON.parse(result).body))
+    // .catch(error => console.log('error', error));
+}
